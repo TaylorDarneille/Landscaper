703e502817c1f849a0d3f9054540f4a173554f3a
Juliana Fernandes <julianafernandes@Albertos-MacBook-Pro.local>
2020-09-02 23:06:53 -0300

    landscaper: user can buy scissors

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-let money = 0;
+let money = 5 ;
+let rustyScissor = false;
+
 const start = () => {
-    alert("You can cut grass with teeth!");
-    useTeeth();
+    alert("You can cut grass with teeth!")
+    useTeeth()
     
 }
+
 const useTeeth = () => {
     let usingTeeth = prompt("Want to spend the day cutting grass with your teeth?","Yes or No")
-    if (usingTeeth === "Yes"){
+    if (usingTeeth === "Yes" && (money > 5 || money < 5)){
       money++
       alert("Money: $"+ money)
       start()
+    } else if (usingTeeth === "Yes" && money === 5){
+        getScissor()
     } else if (usingTeeth === "No"){
         start()
+ }
+}
+
+const getScissor = () => {
+    let gettingScissor = prompt("Do you want to buy a pair of rusty scissors?","Yes or No")
+    if (gettingScissor === "Yes"){
+        money -= 5
+        rustyScissor = true 
+        alert("Money: $| "+ money+" You have a pair of rusty scissors")
+        start()
+      }else if (gettingScissor === "No"){
+          start()
     }
 }
 start()
+
