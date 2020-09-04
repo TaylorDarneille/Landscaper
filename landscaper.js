4d1e7b38dc2f9ac94e6e493c474ec104ba9c9496
elizchan <lizchan@bu.edu>
2020-09-03 22:56:57 -0400

    landscaper: user can use teeth to cut grass

---
+++ landscaper.js
@@ -0,0 +1,18 @@
+let userInput = prompt("Did you mow the lawn with your teeth?", "yes or no?")
+
+if(userInput === "Yes" || userInput === "yes") {
+    alert("Nice!")
+} else if (userInput === "No" || userInput === "no") {
+    alert("No pay!")
+}
+
+//let ifScissors = false
+
+// const getScissors = () => {
+//     if(ifScissors === false) {
+//         alert("cut lawn with teeth")
+//     } else if(ifScissors === true) {
+//         alert("")
+//     }
+    
+// }
