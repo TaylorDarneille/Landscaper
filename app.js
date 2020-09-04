7aae026bfae8f045ea09b63bd9dcee05350a2f83
justin-nykiel <justin.nykiel@gmail.com>
2020-09-04 18:12:37 -0500

    made a grid layout, set start button to fade out on click

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
 let playGame;
 let bankRoll = 0;
-document.addEventListener('DOMContentLoaded', function () {
-    let startButton = document.querySelector('#startButton');
+document.addEventListener("DOMContentLoaded", function () {
+  const startButton = document.querySelector("#startButton");
 
-    let start = () => {
-        playGame = prompt("Would you like to play a game?", "yes/no");
-        let button1 = document.querySelector('#button1');
-        if(playGame === "yes") {
-        button1.innerHTML = "<button>Click me to chomp grass for a whole day for $1</button>";
-    }
-    }
-    
-    startButton.addEventListener("click", start);
+  //function to start game when click
+  const start = () => {
+    const button1 = document.querySelector("#button1");
+    button1.innerHTML = "<button>Click me to chomp grass for a whole day for $1</button>";
 
-    let teethButton = document.querySelector('#button1');
-    let bank = document.querySelector('#bankRoll');
-    let cutTeeth = () => {
-        bankRoll++;
-        bank.innerText = bankRoll;
+    const landscaper = document.createElement("h1");
+    landscaper.innerText = "Landscaper";
+    landscaper.setAttribute("id", "landscaper")
+    fadeOut(startButton);
+    //startButton.parentNode.replaceChild(landscaper, startButton);
+  };
+  //function to fadeout elements
+  function fadeOut(fadee) {
+    setInterval(function() { hide(fadee); }, 100);
+  }
+  function hide(fadee) {
+    let opacity = Number(window.getComputedStyle(fadee).getPropertyValue("opacity"));
+    if (opacity > 0) {
+      opacity = opacity - 0.1;
+      fadee.style.opacity = opacity;
+    } else {
+      startButton.remove();
+      fadeIn(landscaper);
+      clearInterval(intervalID);
     }
-    teethButton.addEventListener('click', cutTeeth);
+  }
+  //function to fadein the title
+  function fadeIn(waker) {
+    setInterval(function() { reveal(waker); }, 100);
+  }
+  function reveal(waker) {
+    let opacity = Number(window.getComputedStyle(waker).getPropertyValue("opacity"));
+    if (opacity < 1) {
+      opacity = opacity + 0.1;
+      waker.style.opacity = opacity;
+    } else {
+      clearInterval(intervalID);
+    }
+  }
+  startButton.addEventListener("click", start);
+
+  let teethButton = document.querySelector("#button1");
+  let bank = document.querySelector("#bankRoll");
+  let cutTeeth = () => {
+    bankRoll++;
+    bank.innerText = bankRoll;
+  };
+  teethButton.addEventListener("click", cutTeeth);
 });
-
-
