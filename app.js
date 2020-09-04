d86224c0f3400bdd49725c56d3fe48fad6f9730b
regularvanessaperson <vamavila4@gmail.com>
2020-09-04 18:33:20 -0500

    landscaper: user can buy push lawnmower and can use it to cut the grass- by the time I figured it out I had done both

--- app.js
+++ app.js
@@ -20,10 +20,16 @@ const showStatus = () => {
 };
 //what will the actions do?
 const askForAction= () => {
+    checkWinner();
     showStatus();
-    if (money === 5 && tool !== "rustyScissors"){
-        askForScissors();
+    if (money === 5 || money === 25){
+        storeTools();
     }
+    // if (money === 5 && tool !== "rustyScissors"){
+    //     askForScissors();
+    // if(money === 25 && tool !== "oldMower"){
+    //     askForOldMower();
+    // }
     const choice = prompt(
         "Will you cut the lawn?",
         "Yes/No/Restart"
@@ -37,6 +43,13 @@ const askForAction= () => {
         start();
     }
 };
+const storeTools = () => {
+    if (money === 5 && tool !== "rustyScissors"){
+        askForScissors();
+    } else if(money === 25 && tool !== "oldMower"){
+        askForOldMower();
+    }
+}
 
 //this functions has the tools
 const cutLawns = () => {
@@ -44,6 +57,8 @@ const cutLawns = () => {
         eatLawns();
     } else if (tool === "rustyScissors"){
         cutWithScissors();
+    }else if (tool === "oldMower"){
+        cutWithOldMower();
     }
 };
 //adds lawn cut an $1
@@ -63,16 +78,39 @@ const cutWithScissors = () => {
     money += 5;
     askForAction();
 };
+//make the mower add $50 per lawn cut
+const cutWithOldMower = () => {
+    lawnsCut++;
+    money+= 50;
+    askForAction();
+}
 
 //ask if you want scissors
-const askForScissors = () =>{
+const askForScissors = () => {
     const wantScissors = prompt("Do you want to buy some rusty scissors for $5?", "Yes/No");
     if (wantScissors === "Yes"){
-        money -= 5
-        tool = "rustyScissors"
+        money -= 5;
+        tool = "rustyScissors";
         showStatus();
     } else if (wantScissors === "No") {
         askForAction();
     };
+}
+// ask if you want a mower
+const askForOldMower = () => {
+    const wantOldMower = prompt ("Do you want to buy an old-timey push lawnmower for $25?", "Yes/No");
+    if (wantOldMower === "Yes"){
+        money -= 25;
+        tool = "oldMower";
+        showStatus();
+    }else if (wantOldMower === "No") {
+        askForAction();
+    };
+}
+
+const checkWinner = () => {
+    if (money === 500){
+        alert("You are a winner! Great job!");
+    }
 }
 start();
