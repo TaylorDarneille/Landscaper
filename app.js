9e29d4a53cfcf8a5128de942ddda49660805c059
regularvanessaperson <vamavila4@gmail.com>
2020-09-03 23:19:43 -0500

    Landscaper App: user can use scissors to cut grass

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ greet();
 //cut lawns and make $1 forever
 let lawnsCut;
 let money;
-
+let tool;
 // alert("You can cut the lawn with your teeth");
 const start = () => {
     lawnsCut = 0;
     money= 0;
+    tool= "teeth";
     askForAction(); //invoke function
 };
 //shows how many lawns have been cut and how much money is earned
@@ -20,7 +21,7 @@ const showStatus = () => {
 //what will the actions do?
 const askForAction= () => {
     showStatus();
-    if (money === 5){
+    if (money === 5 && tool !== "rustyScissors"){
         askForScissors();
     }
     const choice = prompt(
@@ -37,9 +38,16 @@ const askForAction= () => {
     }
 };
 
-
-//adds lawn cut an $1
+//this functions has the tools
 const cutLawns = () => {
+    if (tool === "teeth"){
+        eatLawns();
+    } else if (tool === "rustyScissors"){
+        cutWithScissors();
+    }
+};
+//adds lawn cut an $1
+const eatLawns = () => {
     lawnsCut++;
     money += 1;
     askForAction();
@@ -49,14 +57,19 @@ const cutLawns = () => {
 const beLazy = () => {
     askForAction();
 };
-//use the scissors
-
+//make the scissors add $5 for each lawn cut
+const cutWithScissors = () => {
+    lawnsCut++;
+    money += 5;
+    askForAction();
+};
 
 //ask if you want scissors
 const askForScissors = () =>{
     const wantScissors = prompt("Do you want to buy some rusty scissors for $5?", "Yes/No");
     if (wantScissors === "Yes"){
         money -= 5
+        tool = "rustyScissors"
         showStatus();
     } else if (wantScissors === "No") {
         askForAction();
