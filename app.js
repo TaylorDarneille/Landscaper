63e556bc180d8f69ed18f7e5f04167662a00604c
monicaleep <monica.parrillo@gmail.com>
2020-09-05 18:22:05 -0500

    landscaper: user can buy push lawnmower

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-
-let tool;
-let money;
-let day;
-const tools = {
+// the tools object holds all the details about the available tools. To start, user only has teeth
+const TOOLS = {
   teeth: {name: "teeth", cost: 0, revenue:1, has: true },
-  scissors: {name: "scissors", cost: 5, revenue: 5, has: false}
+  scissors: {name: "scissors", cost: 5, revenue: 5, has: false},
+  pushmower: {name: "push mower", cost: 25, revenue: 50, has: false}
   }
 
-const start = () => {
-  tool = tools.teeth;
-  money = 0;
-  day = 0;
-  askForAction() //invoking function
-}
+//set active tool to be teeth
+let tool = TOOLS.teeth;
+//initialize day number and money
+let money = 50;
+let day = 0 ;
+
+
+
 
 const showStatus = () => {
   alert(`It is day ${day}. You have ${tool.name} to mow, and $${money}.`)
@@ -20,45 +20,51 @@ const showStatus = () => {
 
 const askForAction = () => {
   showStatus();
-  const choice = prompt("What do you want to do?","cut grass / buy tool");
+  const choice = prompt(`What do you want to do?`,`cut grass / buy tool`);
   if (choice){
     if (choice.toLowerCase() === "cut grass"){
       cutGrass();
     } else if (choice.toLowerCase() === "buy tool"){
       buyTool();
     } else{
-      alert("Not a valid entry")
+      alert(`Not a valid entry`)
     }
+    //the else is if the user hits cancel, so no choice given
   } else {
-    alert("I guess you don't find this game very fun...")
+    alert(`I guess you don't find this game very fun...I promise it is, keep going!`)
   }
 }
+
 const buyTool = () =>{
-  const toolToBuy = prompt("Which tool would you like to buy?","scissors");
-  if(!tools[toolToBuy]){
-    alert("Not a valid entry");
-    askForAction();
-  }
-    else if (tools[toolToBuy].has){
+  const toolToBuy = prompt(`Which tool would you like to buy?`,`scissors/pushmower`);
+  if(!TOOLS[toolToBuy]){
+    alert(`Not a valid entry`);
+    }
+    else if (TOOLS[toolToBuy].has){
     alert(`You already have ${toolToBuy}`);
-    askForAction();
-  } else if (tools[toolToBuy].cost>money){
-    alert(`You don't have enough. ${toolToBuy} costs ${tools[toolToBuy].cost} and you have ${money}`)
-    askForAction();
+
+  } else if (TOOLS[toolToBuy].cost>money){
+    alert(`You don't have enough. ${toolToBuy} costs ${TOOLS[toolToBuy].cost} and you have ${money}`)
+
   } else {
     alert(`You have bought ${toolToBuy}`);
-    tools[toolToBuy].has = true;
-    tool = tools[toolToBuy];
+    TOOLS[toolToBuy].has = true;
+    tool = TOOLS[toolToBuy];
     money -= tool.cost;
-    askForAction();
+
   }
 }
+
+//cut grass adds a day and adds revenue depending on which tool you have
 const cutGrass = () => {
   money += tool.revenue;
   day++;
-  askForAction();
+  alert(`You have earned ${tool.revenue}`)
+
 }
 
 
-
-start()
+// main game engine
+while (money<1000){
+  askForAction();
+}
