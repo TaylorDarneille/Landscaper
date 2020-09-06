e35f07ae2e25b7bd62408dd90faee53d08c82bc3
elizchan <lizchan@bu.edu>
2020-09-05 22:08:07 -0400

    I am able to end the game, however I am still not able to get the second userInput === yes to have the alert to pop up

--- landscaper.js
+++ landscaper.js
@@ -9,65 +9,74 @@
 let wallet = 0
 
 const earnMoney = () => {
-    const userInput = prompt("How did you cut the grass today?");
-    console.log(userInput)
-    if(userInput === "teeth"){
-        wallet += 1;
-        alert("You earned $1");
-    }else if(userInput === "scissors"){
-        wallet += 5;
-        alert("You earned $5");
-    }else if(userInput === "old lawnmower"){
-        wallet += 50;
-        alert("You earned $50");
-    }else if(userInput === "fancy lawnmower"){
-        wallet += 100;
-        alert("You earned $100");
-    }else if(userInput === "starving students"){
-        wallet += 250;
-        alert("You earned $250")
-    }
-};
-earnMoney()
-
-const buyTools = () => {
-  if(wallet >= 5){
-    alert("You can buy rusty scissors")
-  }else if(wallet >= 25){
-    alert("You can buy old lawnmower")
-  }else if(wallet >= 250){
-    alert("You can buy fancy lawnmower")
-  }else if(wallet >= 500){
-    alert("You can buy starving students")
+  let userInput = prompt("How did you cut the grass today?");
+  while(userInput === "teeth"){
+    wallet += 1;
+    alert("You earned $1", "You now have "+ wallet)
+    userInput = prompt("How did you cut the grass today?")
+    if(wallet >= 5){
+      prompt("You can buy rusty scissors!", "Yes or no?")
+    }if(userInput === "yes"){
+        wallet -= 5;
+        alert("You have bought rusty scissors");
+        userInput = prompt("How did you cut the grass today?")
+      }else(
+      alert("Keep working!")
+      )
   }
-}
-buyTools()
-
-// const Lowes = () => {
-//     const buyingTools = prompt(
-//       "Welcome to Lowes - What do you want to buy today?",
-//       "Type: 1.Rusty Scissors | 2.Old lawnmower | 3.Fancy lawnmower | 4.Starving Students"
-//     );
-//     if (buyingTools === "Rusty Scissors" && wallet >= 5) {
-//       userTools.push(rustyScissors);
-//       wallet -= 5;
-//       alert(`Congratulations, you have bought ${tools[1].name}`);
-//     } else if (buyingTools === "Old lawnmower" && wallet >= 25) {
-//       userTools.push(oldMower);
-//       wallet -= 25;
-//       alert(`Congratulations, you have bought ${tools[2].name}`);
-//     } else if (buyingTools === "Fancy lawnmower" && wallet >= 250) {
-//       userTools.push(fancyMower);
-//       wallet -= 250;
-//       alert(`Congratulations, you have bought ${tools[3].name}`);
-//     } else if (buyingTools === "Starving Students" && wallet >= 500) {
-//       userTools.push(starvingStudents);
-//       wallet -= 500;
-//       alert(`Congratulations, you have bought ${tools[4].name}`);
-//     }
-//     mowLawnToday()
-//   };
-  
-if(userInput === "starving students" && wallet === 1000){
+  while(userInput === "rusty scissors"){
+    wallet += 5;
+    alert("You earned $5", "You now have "+ wallet)
+    userInput = prompt("How did you cut the grass today?")
+    if(wallet >= 25){
+      prompt("You can buy old lawnmower", "Yes or no?")
+    }if(userInput === "yes"){
+        wallet -= 25;
+        alert("You have bought old lawnmower");
+        userInput = prompt("How did you cut the grass today?")
+      }else(
+        alert("Keep working!")
+      )
+  }
+  while(userInput === "old lawnmower"){
+    //earn $50 per time user inputs old lawnmower
+    wallet += 50;
+    alert("You earned $50", "You now have "+ wallet)
+    userInput = prompt("How did you cut the grass today?")
+    if(wallet >= 250){
+      prompt("You can buy fancy lawnmower", "Yes or no?")
+    }if(userInput === "yes"){
+        wallet -= 250;
+        alert("You have bought fancy lawnmower");
+        userInput = prompt("How did you cut the grass today?")
+      }else(
+        alert("Keep working!")
+      )
+  }
+  while(userInput === "fancy lawnmower"){
+    //earn $100 per time user inputs fancy lawnmower
+    wallet += 100;
+    alert("You earned $100", "You now have "+ wallet)
+    userInput = prompt("How did you cut the grass today?")
+    if(wallet >= 500){
+      prompt("You can buy starving students", "Yes or no?")
+    }if(userInput === "yes"){
+        wallet -= 500;
+        alert("You have bought starving students");
+        userInput = prompt("How did you cut the grass today?")
+      }else(
+        alert("Keep working!")
+      )
+  }
+  while(userInput === "starving students"){
+    //earn $250 per time user inputs starving students
+    wallet += 250;
+    alert("You earned $250", "You now have "+ wallet)
+    userInput = prompt("How did you cut the grass today?")
+    if(userInput === "starving students" && wallet >= 1000){
       alert("Lanscaper wins!")
+    }
   }
+};
+
+earnMoney()
