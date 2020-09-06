ff8dd541fcd3a8b464c55053f4bc0978911c7477
Steven Edwards <stevenedwards@MacBook-Pro.local>
2020-09-06 15:55:26 -0400

    landscaper: user can buy scissors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const opening = () => {
         
 let teethPrompt = prompt('Welcome to Landscaping. You have $' + money + ' . What tool would you like to use?', )
     if (teethPrompt === 'teeth') {
-        alert('you will be using your teeth for the day');
+        alert('you will be using your teeth and will earn $1 for the day.');
         money++;
         document.querySelector('.money').innerHTML = money;
     } else if (teethPrompt !== 'teeth') {
@@ -22,3 +22,13 @@ let teethPrompt = prompt('Welcome to Landscaping. You have $' + money + ' . What
 }
 };
 opening();
+
+const buyScissors = () => {
+    alert('You purchased rusty scissors for $5. You may do this just once');
+    money -= 5;
+    document.querySelector('.money').innerHTML = money;
+}
+let rustyScissorsButton = document.querySelector('.rustyScissors');
+rustyScissorsButton.addEventListener('click', buyScissors);
+
+//continues the game after buying the pair of rusty scissors 
