cf9e435fc75a471aa7374116d9586d21d05152c6
justin-nykiel <justin.nykiel@gmail.com>
2020-09-07 10:48:45 -0500

    added button once  is reached to buy scissors, and then use scissor for /day

--- app.js
+++ app.js
@@ -7,48 +7,75 @@ document.addEventListener("DOMContentLoaded", function () {
   const start = () => {
     const button1 = document.querySelector("#button1");
     button1.innerHTML = "<button>Click me to chomp grass for a whole day for $1</button>";
-
     const landscaper = document.createElement("h1");
     landscaper.innerText = "Landscaper";
     landscaper.setAttribute("id", "landscaper")
-    fadeOut(startButton);
-    //startButton.parentNode.replaceChild(landscaper, startButton);
+    startButton.parentNode.replaceChild(landscaper, startButton);
+    
   };
   //function to fadeout elements
-  function fadeOut(fadee) {
-    setInterval(function() { hide(fadee); }, 100);
-  }
-  function hide(fadee) {
-    let opacity = Number(window.getComputedStyle(fadee).getPropertyValue("opacity"));
-    if (opacity > 0) {
-      opacity = opacity - 0.1;
-      fadee.style.opacity = opacity;
-    } else {
-      startButton.remove();
-      fadeIn(landscaper);
-      clearInterval(intervalID);
-    }
-  }
+  // function fadeOut(fadee) {
+  //   setInterval(function() { hide(fadee); }, 100);
+  // }
+  // function hide(fadee) {
+  //   let opacity = Number(window.getComputedStyle(fadee).getPropertyValue("opacity"));
+  //   if (opacity > 0) {
+  //     opacity = opacity - 0.1
+  //     fadee.style.opacity = opacity;
+  //   } else {
+      
+      
+  //     // fadeIn(landscaper);
+  //     clearInterval(intervalID);
+  //   }
+  // }
   //function to fadein the title
-  function fadeIn(waker) {
-    setInterval(function() { reveal(waker); }, 100);
-  }
-  function reveal(waker) {
-    let opacity = Number(window.getComputedStyle(waker).getPropertyValue("opacity"));
-    if (opacity < 1) {
-      opacity = opacity + 0.1;
-      waker.style.opacity = opacity;
-    } else {
-      clearInterval(intervalID);
-    }
-  }
+  // function fadeIn(waker) {
+  //   setInterval(function() { reveal(waker); }, 100);
+  // }
+  // function reveal(waker) {
+  //   let opacity = Number(window.getComputedStyle(waker).getPropertyValue("opacity"));
+  //   if (opacity < 1) {
+  //     opacity = opacity + 0.1;
+  //     waker.style.opacity = opacity;
+  //   } else {
+  //     clearInterval(intervalID);
+  //   }
+  // }
   startButton.addEventListener("click", start);
-
+  //function to update bankroll
+  function updateBank () {
+    bank.innerText = bankRoll;
+  }
+  
+  //button for cutting grass with teeth
   let teethButton = document.querySelector("#button1");
   let bank = document.querySelector("#bankRoll");
-  let cutTeeth = () => {
+  let counter = 0;
+  const cutTeeth = () => {
     bankRoll++;
-    bank.innerText = bankRoll;
+    updateBank();
+    if(bankRoll === 5 && counter === 0){
+      const butt2 = document.querySelector("#button2");
+      butt2.innerHTML = "<button>Click me to buy Scissors for $5</button>";
+      butt2.setAttribute("id", "button2");
+      counter ++;
+    }
   };
   teethButton.addEventListener("click", cutTeeth);
+  //function for buying scissors and using, if bought once, cannout buy again
+  let boughtScissors = false;
+  const buyUseSciss = document.querySelector("#button2");
+  const buyUseScissors = () => {
+    if(boughtScissors === false) {
+      boughtScissors = true;
+      bankRoll = bankRoll - 5;
+      updateBank();
+      document.querySelector("#button2").innerHTML = "<button>Cut grass with scissors to earn $5 a day</button> ";
+    } else {
+      bankRoll += 5;
+      updateBank();
+    }
+  }
+  buyUseSciss.addEventListener("click", buyUseScissors);
 });
