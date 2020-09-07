5d1fdc3666bf794d5b7505556e971d0a42d71363
NatrezC <cnatrez@gmail.com>
2020-09-07 08:20:11 -0500

    Landscaper: ask for pushmower

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-let lawn;
 let dollar;
 let scissors;
 let pushmower;
@@ -8,7 +7,6 @@ let team;
 //cut grass with teeth
 const start = () => {
     dollar = 0;
-    lawn = 0;
     scissors = 0;
     pushmower = 0;
     powermower = 0;
@@ -26,21 +24,49 @@ const myAccount = () => {
 
 //You would have to pay $5 for rusty scissors
 const goToStore = () => {
-    const choice = prompt("Welcome to Lowes! What would you like?", "scissors, push mower, power mower, or nothing");
-    if ((choice === "scissors" || choice === "Scissors") && dollar > 5) {
+    const choice = prompt("Welcome to Lowes! What would you like?", "scissors, push mower, powermower, a team or nothing");
+    if ((choice === "scissors" || choice === "Scissors") && dollar >= 5) {
         alert("That would be $5");
         scissors++;
         dollar -= 5;
         myAccount();
-        
-    } else if ((choice === "scissors" || choice === "Scissors" )&& dollar < 5) {
+    } else if ((choice === "scissors" || choice === "Scissors") && dollar < 5) {
         alert("You do not have enough for scissors");
         goToStore();
-    } if (choice === "yes" || choice === "Yes" && dollar < 5) {
-        alert("Aw man! You do not have enough yet");
+        //buy pushmower
+    } else if ((choice === "pushmower" || choice === "Pushmower") && dollar >= 25) {
+        alert("That would be $25");
+        scissors++;
+        dollar -= 25;
+        myAccount();
+    } else if ((choice === "pushmower" || choice === "Pushmower") && dollar < 25) {
+        alert("You do not have enough for pushmower");
+        goToStore();
+    } else if ((choice === "powermower" || choice === "Powermower") && dollar > 5) {
+        alert("That would be $250");
+        scissors++;
+        dollar -= 250;
+        myAccount();
+    } else if ((choice === "powermower" || choice === "Powermower") && dollar < 5) {
+        alert("You do not have enough for powermower");
+        goToStore();
+    } else if ((choice === "team" || choice === "Team") && dollar > 5) {
+        alert("That would be $5");
+        scissors++;
+        dollar -= 5;
+        myAccount();
+    } else if ((choice === "team" || choice === "Team") && dollar < 5) {
+        alert("You do not have enough for team");
+        goToStore();
+    } else if (choice === "nothing" || choice === "Nothing") {
+        alert("Well have a nice day!");
         myAccount();
     }
+        
 }
+
+
+
 
 //Ask for service if you want you lawn cut
 const  askForService = () => {
@@ -53,21 +79,44 @@ const  askForService = () => {
     }
 } 
 
-//cut grass for $1
 const buyService = () => {
-   const choice = prompt("What service would you like?", "$1 teeth, $5 scissors, $25 push mower, or $100 power mower");
+    const choice = prompt("What service would you like?", "$1 teeth, $5 scissors, $25 push mower, or $100 power mower, or nothing");
     if (choice === "teeth" || choice === "teeth") {
         alert("That would be $1 dollar please");
+        //cut grass for $1
+        dollar++;
+        //ask for scissors $5
     } else if ((choice === "scissors" || choice === "Scissors") && scissors >= 1) {
         alert("Ok! That would be $5");
-    } else if ((choice === "scissors" || choice === "Scissors") && scissors < 1) {
+        dollar += 5;
+    } else if ((choice === "scissors" || choice === "Scissors") && scissors < 1) {// if user even has item
         alert("It looks like i need to go to the store")
         goToStore();
+        //ask for pushmower $25
+    } else if ((choice === "pushmower" || choice === "Pushmower") && pushmower >= 1) {
+        alert("Great! That would be $25");
+        dollar += $25;
+    } else if ((choice === "pushmower" || choice === "Pushmower") && pushmower < 1) {
+        alert("It looks like i need to go to the store")
+        goToStore();
+    } else if ((choice === "powermower" || choice === "Powermower") && powermower >= 1) {
+        alert("Great! That would be $100")
+        dollar += 100;
+    } else if ((choice === "powermower" || choice === "Powermower") && powermower >= 1) {
+        alert("Great! That would be $25")
+    } else if ((choice === "team" || choice === "Team") && team >= 1) {
+        alert("Great! That would be $250")
+        dollar += 250;
+    } else if ((choice === "team" || choice === "Team") && team < 1) {
+        alert("Great! That would be $25")
+        goToStore();
+    } else if (choice === "nothing" || choice === "Nothing") {
+        alert("Well have a nice day!")
+        myAccount();
     }
-    //Want it to prompt that how many lawns has been cut 
-    //Add another dollar for each lawn that has been cut
-    dollar++;
-    lawn++;
+    
+    
+    
     
     myAccount();
 }
