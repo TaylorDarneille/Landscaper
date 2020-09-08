a53aee053bc2e7b2d644d7353c05114c61be0828
evab1209 <evaburbacki@gmail.com>
2020-09-08 00:19:23 -0400

    landscaper: win scenario

--- app.js
+++ app.js
@@ -1,49 +1,65 @@
 let bank = 0;
 const balanceHTMLElement = document.getElementById("bank");
 function checker(tools) {
+    // if we have enough money in the bank to buy rusty scissors and dont have 1 already
     if (bank >= tools.rustyScissors.buy.cost && tools.rustyScissors.quantity < tools.rustyScissors.limit) {
         tools.rustyScissors.buy.element.disabled = false;
     }
+    // if we have enough money in the bank to buy oldtimeylawnmower and have rusty scissors
     if (bank >= tools.oldTimeyLawnmower.buy.cost && tools.rustyScissors.quantity > 0) {
         tools.oldTimeyLawnmower.buy.element.disabled = false;
     }
+    // if we have enough money in the bank to buy poweredlawnmower and have old timey
     if (bank >= tools.poweredLawnmower.buy.cost && tools.oldTimeyLawnmower.quantity > 0) {
         tools.poweredLawnmower.buy.element.disabled = false;
     }
+    //if we have enough money in bank to hire students and have a powered lawnmower
     if (bank >= tools.starvingStudents.buy.cost && tools.poweredLawnmower.quantity > 0) {
         tools.starvingStudents.buy.element.disabled = false;
     }
 
     // if conditions that apply to disabling by limit
+    // if we already have rusty scissors
     if (tools.rustyScissors.quantity === tools.rustyScissors.limit) {
         tools.rustyScissors.buy.element.disabled = true;
         tools.rustyScissors.use.element.disabled = false;
     }
+    // if we already have rusty old timey
     if (tools.oldTimeyLawnmower.quantity === tools.oldTimeyLawnmower.limit) {
         tools.oldTimeyLawnmower.buy.element.disabled = true;
         tools.oldTimeyLawnmower.use.element.disabled = false;
     }
+    // if we already have powered lawnmower
     if (tools.poweredLawnmower.quantity === tools.poweredLawnmower.limit) {
         tools.poweredLawnmower.buy.element.disabled = true;
         tools.poweredLawnmower.use.element.disabled = false;
     }
+    // if we already have starving students
     if (tools.starvingStudents.quantity === tools.starvingStudents.limit) {
         tools.starvingStudents.buy.element.disabled = true;
         tools.starvingStudents.use.element.disabled = false;
     }
-    
+    if (tools.starvingStudents.quantity > 0 && bank >= 1000) {
+        // sometimes, alert would get executed before the HTML element representing my bank balance
+        // has finished updating. This is why I am adding a delay of 300 ms so the change in balance
+        // can reflect before the alert window is shown
+        setTimeout(function() {
+            alert("You've Won!!");
+        }, 300);
+    }
 }
 function increaseBankBalance(amountOfMoney, tools) {
-    bank = bank + amountOfMoney;
+    bank += amountOfMoney; // same as bank = bank + 1
     balanceHTMLElement.innerHTML = bank;
     checker(tools);
 }
 function decreaseBankBalance(amountOfMoney, tools) {
-    bank = bank - amountOfMoney;
+    bank -= amountOfMoney; // same as bank = bank - 1
     balanceHTMLElement.innerHTML = bank;
     checker(tools);
 }
 document.addEventListener("DOMContentLoaded", function(event) {   
+    // initialize HTML objects
     balanceHTMLElement.innerHTML = bank;
     const useTeeth = document.getElementById("use-teeth");
     const buyScissors = document.getElementById("buy-rusty-scissors");
@@ -54,11 +70,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const usePoweredLawnmower = document.getElementById("use-battery-powered-lawnmower");
     const hireTeamStarvingStudents = document.getElementById("hire-team-starving-students");
     const useTeamStarvingStudents = document.getElementById("use-team-starving-students");
+
+    // define our toolbox data structure using an objects and properties
     const tools = {
         teeth: {
             use: {
                 element: useTeeth,
-                isDisabled: false,
                 servicePrice: 1,
             },
             buy: null,
@@ -68,12 +85,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
         rustyScissors: {
             use: {
                 element: useScissors,
-                isDisabled: true,
                 servicePrice: 5,
             },
             buy: {
                 element: buyScissors,
-                isDisabled: true,
                 cost: 5,
             },
             quantity: 0,
@@ -82,12 +97,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
         oldTimeyLawnmower: {
             use: {
                 element: useOldTimeyLawnmower,
-                isDisabled: true,
                 servicePrice: 50,
             },
             buy: {
                 element: buyOldTimeyLawnmower,
-                isDisabled: true,
                 cost: 25,
             },
             quantity: 0,
@@ -96,12 +109,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
         poweredLawnmower: {
             use: {
                 element: usePoweredLawnmower,
-                isDisabled: true,
                 servicePrice: 100,
             },
             buy: {
                 element: buyPoweredLawnmower,
-                isDisabled: true,
                 cost: 250,
             },
             quantity: 0,
@@ -110,21 +121,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
         starvingStudents: {
             use: {
                 element: useTeamStarvingStudents,
-                isDisabled: true,
                 servicePrice: 250,
             },
             buy: {
                 element: hireTeamStarvingStudents,
-                isDisabled: true,
                 cost: 500,
             },
             quantity: 0,
             limit: 1,
         },
     };
-    // document.getElementById("use-teeth").addEventListener("click", function(event) {
-    //    increaseBankBalance(1);
-    // });
+
     tools.teeth.use.element.addEventListener("click", function(event) {
         increaseBankBalance(tools.teeth.use.servicePrice, tools);
     });
